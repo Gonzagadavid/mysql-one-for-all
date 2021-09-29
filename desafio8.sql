a0889d81ee062bb3861b574238f214f64820daae
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-09-29 11:20:28 -0300

    Corrigido erros de formatacao apontados pelo code climate

--- desafio8.sql
+++ desafio8.sql
@@ -7,8 +7,8 @@ BEFORE DELETE ON Usuario
 FOR EACH ROW
 BEGIN
 SET SQL_SAFE_UPDATES = 0;
-     DELETE FROM Historico_reproducao WHERE usuario_id = old.usuario_id;
-     DELETE FROM Seguindo_artista WHERE usuario_id = old.usuario_id;
+      DELETE FROM Historico_reproducao WHERE usuario_id = old.usuario_id;
+      DELETE FROM Seguindo_artista WHERE usuario_id = old.usuario_id;
 SET SQL_SAFE_UPDATES = 1;
 END $$
 
